05491636be348f2cc5c90e0ce7d3079b5452c643
David Morley <david@davidmorley.me>
2012-12-21 13:48:52 -0600

    Added public constructor with arguments

--- src/test/java/liquibase/change/ext/AddSynonymChangeTest.java
+++ src/test/java/liquibase/change/ext/AddSynonymChangeTest.java
@@ -22,27 +22,27 @@ public class AddSynonymChangeTest {
     private static final String CONFIRMATION_MESSAGE_FULL_SYNONYM = CONFIRMATION_MESSAGE_START + FULL_SYNONYM + CONFIRMATION_MESSAGE_MIDDLE + SOURCE_TABLE + CONFIRMATION_MESSAGE_END;
     private static final String CONFIRMATION_MESSAGE_FULL_SOURCE_TABLE = CONFIRMATION_MESSAGE_START + SYNONYM + CONFIRMATION_MESSAGE_MIDDLE + FULL_SOURCE_TABLE + CONFIRMATION_MESSAGE_END;
 
-    @Test(expected = AssertionError.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testGetConfirmationMessageWithNullSourceTable() {
-        AddSynonymChange addSynonymChange = createAddSynonymChange(SOURCE_SCHEMA, null, SCHEMA, SYNONYM);
+        AddSynonymChange addSynonymChange = new AddSynonymChange(SOURCE_SCHEMA, null, SCHEMA, SYNONYM);
         addSynonymChange.getConfirmationMessage();
     }
 
-    @Test(expected = AssertionError.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testGetConfirmationMessageWithNullSynonym() {
-        AddSynonymChange addSynonymChange = createAddSynonymChange(SOURCE_SCHEMA, SOURCE_TABLE, SCHEMA, null);
+        AddSynonymChange addSynonymChange = new AddSynonymChange(SOURCE_SCHEMA, SOURCE_TABLE, SCHEMA, null);
         addSynonymChange.getConfirmationMessage();
     }
 
-    @Test(expected = AssertionError.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testGetConfirmationMessageWithBlankSourceTable() {
-        AddSynonymChange addSynonymChange = createAddSynonymChange(SOURCE_SCHEMA, BLANK, SCHEMA, SYNONYM);
+        AddSynonymChange addSynonymChange = new AddSynonymChange(SOURCE_SCHEMA, BLANK, SCHEMA, SYNONYM);
         addSynonymChange.getConfirmationMessage();
     }
 
-    @Test(expected = AssertionError.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testGetConfirmationMessageWithBlankSynonym() {
-        AddSynonymChange addSynonymChange = createAddSynonymChange(SOURCE_SCHEMA, SOURCE_TABLE, SCHEMA, BLANK);
+        AddSynonymChange addSynonymChange = new AddSynonymChange(SOURCE_SCHEMA, SOURCE_TABLE, SCHEMA, BLANK);
         addSynonymChange.getConfirmationMessage();
     }
 
@@ -51,26 +51,17 @@ public class AddSynonymChangeTest {
         String confirmationMessage = null;
         AddSynonymChange addSynonymChange = null;
 
-        addSynonymChange = createAddSynonymChange(SOURCE_SCHEMA, SOURCE_TABLE, SCHEMA, SYNONYM);
+        addSynonymChange = new AddSynonymChange(SOURCE_SCHEMA, SOURCE_TABLE, SCHEMA, SYNONYM);
         confirmationMessage = addSynonymChange.getConfirmationMessage();
         assertEquals("Confirmation message should properly reflect all components", CONFIRMATION_MESSAGE_ALL_COMPONENTS, confirmationMessage);
 
-        addSynonymChange = createAddSynonymChange(null, SOURCE_TABLE, SCHEMA, SYNONYM);
+        addSynonymChange = new AddSynonymChange(null, SOURCE_TABLE, SCHEMA, SYNONYM);
         confirmationMessage = addSynonymChange.getConfirmationMessage();
         assertEquals("Confirmation message should properly reflect fully-qualified synonym", CONFIRMATION_MESSAGE_FULL_SYNONYM, confirmationMessage);
 
-        addSynonymChange = createAddSynonymChange(SOURCE_SCHEMA, SOURCE_TABLE, null, SYNONYM);
+        addSynonymChange = new AddSynonymChange(SOURCE_SCHEMA, SOURCE_TABLE, null, SYNONYM);
         confirmationMessage = addSynonymChange.getConfirmationMessage();
         assertEquals("Confirmation message should properly reflect fully-qualified source table", CONFIRMATION_MESSAGE_FULL_SOURCE_TABLE, confirmationMessage);
-    }
-
-    private AddSynonymChange createAddSynonymChange(String sourceSchema, String sourceTable, String schema, String synonym) {
-        AddSynonymChange addSynonymChange = new AddSynonymChange();
-        addSynonymChange.setSourceSchemaName(sourceSchema);
-        addSynonymChange.setSourceTableName(sourceTable);
-        addSynonymChange.setSchemaName(schema);
-        addSynonymChange.setSynonymName(synonym);
-        return addSynonymChange;
     }
 
 }
